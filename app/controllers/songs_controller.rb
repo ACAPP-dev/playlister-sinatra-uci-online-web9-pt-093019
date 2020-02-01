7245f601fce0ab27b84f48f9938f6c23a334cab6
ACAPP-dev <acapp909@gmail.com>
2020-01-31 20:23:34 -0800

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,22 +1,58 @@
+require 'sinatra/base'
+require 'rack-flash'
+
+
 class SongsController < ApplicationController
 
+    use Rack::Flash
+    
     get '/songs' do
         @songs = Song.all 
         erb :'songs/index'
     end
 
-    get '/songs/:slug' do
-        @song = Song.find_by_slug(params[:slug])
-        #binding.pry
-        erb :'songs/show'
-    end
-
     get '/songs/new' do
+        @genres = Genre.all
         erb :'songs/new'
     end
 
-    get 'songs/:slug/edit' do
+    get '/songs/:slug' do
         @song = Song.find_by_slug(params[:slug])
+        erb :'songs/show'
+    end
+
+    get '/songs/:slug/edit' do
+        @song = Song.find_by_slug(params[:slug])
+        @genres = Genre.all
         erb :'songs/edit'
+    end
+
+    post '/songs' do
+        @song = Song.create(params[:song])
+        artist_find = Artist.find_by_slug(params[:artist][:name])
+        if !artist_find
+            artist = Artist.create(params[:artist])
+            @song.artist = artist
+            @song.save
+        else
+            @song.artist = artist_find
+            @song.save
+        end
+        params[:genres].each do |genre_id|
+            genre_find = Genre.find(genre_id.to_i)
+            @song.genres << genre_find
+            @song.save
+        end
+        @song.save
+        flash[:message] = "Successfully created song."
+        redirect to("/songs/#{@song.slug}")
+    end
+
+    patch '/songs' do
+        @song = Song.find_by_slug(params[:song][:name])
+        Artist.update(params[:artist])
+        @song.save
+        flash[:message] = "Successfully updated song."
+        redirect to("/songs/#{@song.slug}")
     end
 end
